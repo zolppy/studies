a11871d644b4eb2afeff8f87cb868b866efd28cc
zolppy <gabriel.lcifba@gmail.com>
2024-01-03 16:34:46 -0300

    novo curso

---
+++ react-native-utilizando-web-api/src/servicos/requisicoes/repositorios.js
@@ -0,0 +1,27 @@
+import api from "../api";
+
+export async function pegarRepositoriosDoUsuario(id){
+    try {
+        const resultado = await api.get(`/repos?postId=${id}`);
+        return resultado.data
+    }
+    catch (error){
+        console.log(error)
+        return []
+    }
+}
+
+export async function salvarRepositoriosDoUsuario(postId, nome, data, id){
+    try {
+        await api.put(`/repos/${id}`, {
+            name: nome,
+            data: data,
+            postId: postId
+        });
+        return 'sucesso'
+    }
+    catch (error){
+        console.log(error)
+        return 'erro'
+    }
+}
